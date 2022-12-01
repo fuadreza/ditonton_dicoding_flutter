7fffc2f829c2fdcde05043e88fea19b2a82dcb41
fuadreza <fuadreza1011@gmail.com>
2022-12-01 22:40:55 +0700

    add unit testing, dummy data

---
+++ test/helpers/test_helper.dart
@@ -0,0 +1,18 @@
+import 'package:ditonton_dicoding_flutter/common/services/base_api_client.dart';
+import 'package:ditonton_dicoding_flutter/data/datasources/db/database_helper.dart';
+import 'package:ditonton_dicoding_flutter/data/datasources/movie_local_data_source.dart';
+import 'package:ditonton_dicoding_flutter/data/datasources/movie_remote_data_source.dart';
+import 'package:ditonton_dicoding_flutter/domain/repositories/movie_repository.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+
+@GenerateMocks([
+  MovieRepository,
+  MovieRemoteDataSource,
+  MovieLocalDataSource,
+  BaseApiClient,
+  DatabaseHelper,
+], customMocks: [
+  MockSpec<http.Client>(as: #MockHttpClient)
+])
+void main() {}
